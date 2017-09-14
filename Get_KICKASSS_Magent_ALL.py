a4b2d6d68c9dfd5a3ae9434ae01f4875a6e96821
SlovEnt <slovent@163.com>
2017-09-14 11:12:09 +0800

    KICKASSS网站磁力链太少，暂停该脚本

--- Get_KICKASSS_Magent_ALL.py
+++ Get_KICKASSS_Magent_ALL.py
@@ -33,52 +33,89 @@ dcap["phantomjs.page.settings.loadImages"] = False # 禁止加载图片
 driver = webdriver.PhantomJS(executable_path=phantomjsPath,desired_capabilities=dcap)
 
 
+def __Get_KIS_Dict2DB(soup):
+
+    urls = soup.find_all('ul', class_="dropdown dp-middle dropdown-msg upper")
+
+    for url in urls:
+        for urlLine in url:
+            if urlLine != "\n":
+                urlLine = str(urlLine)
+                dataReg = re.compile(
+                    r'''<li class="topMsg"><a href="/(.+?)/"><i class="ka ka16 ka-.+?"></i>(.+?)</a></li>''')
+                rawDatas = dataReg.findall(urlLine)
+
+                for data in rawDatas:
+
+                    strSql = "select count(*) from sys_dict where dict_the = 'kic' and dict_id = '%s' and dict_item='%s' and dict_item_sl='%s';" % (
+                    data[0], data[1], data[1])
+                    rtnCnt = mysqlExe.ExecQuery(strSql.encode('utf-8'))[0][0]
+
+                    if rtnCnt == 0 and data[0] != "new":
+                        strSql = "INSERT INTO sys_dict (dict_the, dict_id, dict_item, dict_item_sl, dict_item_spf) VALUES ('kic', '%s', '%s', '%s', '');" % (
+                        data[0], data[1], data[1])
+                        print(strSql)
+                        mysqlExe.ExecNonQuery(strSql.encode('utf-8'))
+
+def __Get_Kic_Dict(rs_group):
+    if rs_group == "ALL":
+        sqlStr = "select dict_id from sys_dict where dict_the = 'kic';"
+    else:
+        sqlStr = "select dict_id from sys_dict where dict_the = 'kic' and dict_item = '"+rs_group+"';"
+    listDatas = mysqlExe.ExecQuery(sqlStr.encode('utf-8'))
+    groupList = []
+    for singData in listDatas:
+        groupList.append(singData[0])
+    return groupList
+
+def isElementExist(driver):
+    try:
+        driver.find_element_by_xpath('//*[@id="confirm_age"]/div/div[2]/form/button/span').is_displayed()
+        return True
+    except:
+        return False
+
+def __Down_KIS_Magent_Info(subUrl):
+
+    driver.get(subUrl)
+
+    # <button type="submit" class="siteButton bigButton"><span>Yes, let me see it</span></button>
+    yesButton = '''//*[@id="confirm_age"]/div/div[2]/form/button/span'''
+    if isElementExist(yesButton) == True:
+        driver.find_element_by_xpath(yesButton).click()
+
+    pageSourceScript = driver.page_source
+    # print(pageSourceScript)
+    print("-----------------------------------------------------------------------")
+    soup = bs4.BeautifulSoup(pageSourceScript, "html.parser")
+    tr_id_Regexp = re.compile("torrent_\w+_torrents\d+")
+
+    childNodes = soup.find_all("tr", attrs={"id":tr_id_Regexp});
+
+    for childNode in childNodes:
+        print(tmpinfo(),childNode)
+        # print(tmpinfo(),x.text)
+
+
+
 if __name__ == "__main__":
     try:
 
+        # 取本爬虫需要的相关字典信息
         paramInfo = Get_Param_In_DB("CRAW_KIC")
-
         rootUrl = paramInfo["KIC_ROOT_SITE"]
 
-        wait = ui.WebDriverWait(driver, 10)
+        # driver.get(rootUrl)
+        # pageSourceScript = driver.page_source
+        # soup = bs4.BeautifulSoup(pageSourceScript, "html.parser")
+        # __Get_KIS_Dict2DB(soup) # 从网站爬字典项
 
-        driver.get(rootUrl)
+        subUrls = __Get_Kic_Dict("XXX")
 
-        pageSourceScript = driver.page_source
-
-        soup = bs4.BeautifulSoup(pageSourceScript, "html.parser")
-
-
-        # print(soup)
-
-        # soup.head.children
-
-        urls = soup.find_all('ul', class_="dropdown dp-middle dropdown-msg upper")
-
-        for x in urls:
-            print(tmpinfo(),type(x.text))
-            print(tmpinfo(),x.text)
-
-        # dataReg = re.compile(
-        #     r'''<i class="ka ka16 ka-(.+?) lower"></i>(.+?)</a></li>''')
-        #
-        # rawDatas = dataReg.findall(urls)
-        # print(tmpinfo(),rawDatas)
-        # for data in rawDatas:
-        #
-        #     strSql = "select count(*) from sys_dict where dict_the = 'kic' and dict_id = '%s' and dict_item='%s' and dict_item_sl='%s';" % (data[0],data[1],data[1])
-        #     rtnCnt = mysqlExe.ExecQuery(strSql.encode('utf-8'))[0][0]
-        #
-        #     if rtnCnt == 0:
-        #
-        #         strSql = "INSERT INTO sys_dict (dict_the, dict_id, dict_item, dict_item_sl, dict_item_spf) VALUES ('kic', '%s', '%s', '%s', '');" % (data[0],data[1],data[1])
-        #         print(strSql)
-        #         mysqlExe.ExecNonQuery(strSql.encode('utf-8'))
-
-
-        # wait.until(lambda dr: dr.find_element_by_id('fb-root').is_displayed())
-
-        # print(pageSourceScript)
+        # 启动爬虫进程
+        for subUrl in subUrls:
+            subUrl = "%s/%s/" % (rootUrl, subUrl)
+            __Down_KIS_Magent_Info(subUrl)
 
 
 
