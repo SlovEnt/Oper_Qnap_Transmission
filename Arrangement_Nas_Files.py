585b1f88bb15d199bf84cdc873866bf3f938191d
SlovEnt <slovent@163.com>
2017-09-22 17:13:34 +0800

    xm 1

--- Arrangement_Nas_Files.py
+++ Arrangement_Nas_Files.py
@@ -1,4 +1,5 @@
 import os
+import time
 import paramiko
 import hashlib
 
@@ -19,36 +20,68 @@ def __Get_File_MD5(singFile):
     f.close()
     return myhash.hexdigest()
 
+def __Ssh_Login_Nas():
+    sshTran = paramiko.Transport("xm.imchs.com", 22)
+    sshTran.connect(username="root", password="Xm#Toor.1017")
+    chan = sshTran.open_session()
+    chan.settimeout(60)
+    chan.get_pty()
+    chan.invoke_shell()
+    # sendCmd = "rsync -av --progress /userdisk/data/transmission/downloads/.  admin@192.168.1.201:/share/Datas/Bad_Item/XM_Transmission/%s" % ("NEW_"+ str("%06d") % fld)
+    sendCmd = "rsync -av --progress /userdisk/data/transmission/downloads/.  admin@192.168.1.201:/share/Datas/Bad_Item/XM_Transmission/%s" % ("NEW_"+ str("%06d") % fld)
+    chan.send(sendCmd + "\n")
+    time.sleep(1)
+    chan.send("admin\n")
+    time.sleep(2)
+
+    rtnStr = str(chan.recv(65535),encoding="UTF-8",errors="ignore")
+    print(rtnStr)
 
 
 
 
 if __name__ == "__main__":
     print()
-    rootDir = r"\\192.168.1.201\Datas\Bad_Item"
-    for strRoot,lsDir,lsFiles in os.walk(rootDir):
-        for strFile in lsFiles:
 
-            singFullPathFileExt = os.path.join(strRoot,strFile)
-            # print(debug(),__Get_File_MD5(singFullPathFileExt),strRoot,strFile)
+    paramInfo = Get_Param_In_DB("NAS_CONN")
 
-            convRootPath = strRoot.replace(r"\\","\\\\")
+    print(paramInfo)
 
-            strSql = "select count(*) from nas_files where `allpath` = '%s' and `file_name` = '%s' and  `md5_string` = '%s' ;" % (convRootPath, strFile, __Get_File_MD5(singFullPathFileExt))
-            # print(debug(),strSql)
-            rtnCnt = mysqlExe.ExecQuery(strSql.encode('utf-8'))[0][0]
-            if rtnCnt == 0:
-                strSql = "select count(*) from nas_files where `md5_string` = '%s' ;" % (__Get_File_MD5(singFullPathFileExt))
-                rtnCnt = mysqlExe.ExecQuery(strSql.encode('utf-8'))[0][0]
-                if rtnCnt != 0:
-                    flag = 1 # 已存在相同文件
-                else:
-                    flag = 0
-                strSql = "INSERT INTO nas_files (`allpath`, `file_name`, `md5_string`, flag) VALUES ('%s', '%s', '%s', '%s');" % (convRootPath.replace("\\","\\\\"), strFile, __Get_File_MD5(singFullPathFileExt),flag)
-                print(debug(),strSql)
-                mysqlExe.ExecNonQuery(strSql.encode('utf-8'))
-            else:
-                print(debug(), r"跳过 %s" % (singFullPathFileExt))
+    nasHost = paramInfo[""]
+
+
+    # rootUrl = paramInfo["KIC_ROOT_SITE"]
+
+
+
+
+
+
+
+    # rootDir = r"\\192.168.1.201\Datas\Bad_Item"
+    # for strRoot,lsDir,lsFiles in os.walk(rootDir):
+    #     for strFile in lsFiles:
+    #
+    #         singFullPathFileExt = os.path.join(strRoot,strFile)
+    #         # print(debug(),__Get_File_MD5(singFullPathFileExt),strRoot,strFile)
+    #
+    #         convRootPath = strRoot.replace(r"\\","\\\\")
+    #
+    #         strSql = "select count(*) from nas_files where `allpath` = '%s' and `file_name` = '%s' and  `md5_string` = '%s' ;" % (convRootPath, strFile, __Get_File_MD5(singFullPathFileExt))
+    #         # print(debug(),strSql)
+    #         rtnCnt = mysqlExe.ExecQuery(strSql.encode('utf-8'))[0][0]
+    #         if rtnCnt == 0:
+    #             strSql = "select count(*) from nas_files where `md5_string` = '%s' ;" % (__Get_File_MD5(singFullPathFileExt))
+    #             rtnCnt = mysqlExe.ExecQuery(strSql.encode('utf-8'))[0][0]
+    #             if rtnCnt != 0:
+    #                 flag = 1 # 已存在相同文件
+    #             else:
+    #                 flag = 0
+    #             strSql = "INSERT INTO nas_files (`allpath`, `file_name`, `md5_string`, flag) VALUES ('%s', '%s', '%s', '%s');" % (convRootPath.replace("\\","\\\\"), strFile, __Get_File_MD5(singFullPathFileExt),flag)
+    #             print(debug(),strSql)
+    #             mysqlExe.ExecNonQuery(strSql.encode('utf-8'))
+    #         else:
+    #             print(debug(), r"跳过 %s" % (singFullPathFileExt))
 
 
 
