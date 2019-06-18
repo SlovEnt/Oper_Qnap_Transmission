f9634d101ee86896e62c3002aa8585986371ffb1
SlovEnt <slovent@163.com>
2019-06-18 23:54:47 +0800

    ADD 1.01

--- Down_TPB_ALL_Magent/main.py
+++ Down_TPB_ALL_Magent/main.py
@@ -8,6 +8,7 @@ import time
 import random
 import platform
 from multiprocessing import Pool
+from collections import OrderedDict
 import traceback
 from tpb_proc import tpb_proc_class
 from tpb_proc import globParaList, mysqlConn
@@ -15,6 +16,7 @@ from ResPacks.get_url_html import chrome_get_html_all_content, get_html_all_cont
 from bs4 import BeautifulSoup
 
 tpc = tpb_proc_class(mysqlConn,globParaList["TPB_ROOT_URL"])
+tableName = "get_tpb_all_magnet"
 
 def down_tpb_magent_info(singCategory):
 
@@ -24,16 +26,74 @@ def down_tpb_magent_info(singCategory):
     for i in range(0, pageMaxNum):
 
         enterHttpUrl = "{0}/browse/{1}/{2}/3".format(siteWeb, singCategory, i)
-        subPostHtml = get_html_all_content(enterHttpUrl, "searchResult", "utf-8")
+        subPostHtml = chrome_get_html_all_content(enterHttpUrl, "searchResult", "utf-8")
         soup = BeautifulSoup(subPostHtml, 'html.parser')
 
         nodes = soup.find_all(name="table", attrs={"id": "searchResult"})[0].find_all(name="tr")
-
+        # nodes = soup.find_all(name="div", attrs={"class": "detName"})
+        n = 0
         for node in nodes:
-            print(node)
+            '''
+            `up_datetime` datetime DEFAULT NULL,
+            `hash_string` varchar(128) NOT NULL,
+            `up_user` varchar(255) DEFAULT NULL,
+            `rs_name` varchar(1024) NOT NULL DEFAULT '',
+            `rs_category` varchar(64) DEFAULT NULL,
+            `rs_type` varchar(64) DEFAULT NULL,
+            `up_size` varchar(12) DEFAULT NULL,
+            `magnet` blob NOT NULL,
+            `down_flag` char(1) DEFAULT '0',
+            '''
 
+            tpbDict = OrderedDict()
 
+            if "detName" not in str(node):
+                continue
 
+            n += 1
+
+            # print(node)
+            tpbDict["rs_name"] = node.find_all(name="div")[0].a.text
+            tpbDict["rs_name"] = tpbDict["rs_name"].replace("'", '"')
+            tpbDict["rs_name"] = tpbDict["rs_name"].strip()
+
+            tpbDict["magnet"] = node.find_all(name="td")[1].find_all(name="a")[1]["href"]
+
+            tpbDict["hash_string"] = re.compile(r'''urn:btih:(.+?)&''').findall(tpbDict["magnet"])[0]
+
+            # 加一个&结尾 便于后面正则表达式取值
+            otherStr = "{0}&".format(node.find_all(name="td")[1].find_all(name="font")[0].text)
+            otherStr = otherStr.replace("\xa0"," ")
+
+            tpbDict["rs_category"] = node.find_all(name="td")[0].center.find_all(name="a")[0].text
+
+            tpbDict["rs_type"] = node.find_all(name="td")[0].center.find_all(name="a")[1].text
+
+            tpbDict["up_datetime"], tpbDict["up_size"], tpbDict["up_user"] = re.compile(r'''Uploaded (.+?), Size (.+?), ULed by (.+?)&''').findall(otherStr)[0]
+            print(tpbDict["up_datetime"])
+            tpbDict["up_datetime"] = tpc.format_datetime(tpbDict["up_datetime"])
+
+            tpbDict["down_flag"] = 0
+
+            # print("99884", n, tpbDict)
+
+            whereDict = OrderedDict()
+            whereDict["hash_string"] = tpbDict["hash_string"]
+            rtnData = tpc.table_row_is_exist(tableName, whereDict)
+
+            # 通过触发器取下一个计数号
+            totalNum = tpc.Get_SEQ_Next_Val("TPB_ALL_SEQ")
+
+            if int(rtnData["CNT"]) != 0:
+                print("11598 {2} : {0} {3} {4} {1} 已经存在".format(tpbDict["hash_string"], tpbDict["rs_name"], totalNum, tpbDict["rs_category"], tpbDict["rs_type"], ))
+                continue
+
+            rtnMsg = tpc.insert_row_table(tableName, tpbDict)
+
+            if rtnMsg is True:
+                print("11597 {2} : {0} {3} {4} {1} 已插入后台数据表".format(tpbDict["hash_string"], tpbDict["rs_name"], totalNum, tpbDict["rs_category"], tpbDict["rs_type"],))
+            else:
+                print("11596 {2} : {0} {3} {4} {1} 插入后台数据表失败".format(tpbDict["hash_string"], tpbDict["rs_name"], totalNum, tpbDict["rs_category"], tpbDict["rs_type"],))
 
 
 
--- Down_TPB_ALL_Magent/tpb_proc.py
+++ Down_TPB_ALL_Magent/tpb_proc.py
@@ -6,6 +6,10 @@ import os
 from selenium import webdriver
 from ResPacks.UsedCommFuncs import Get_Param_Info
 from ResPacks import torndb
+import datetime
+import re
+from collections import OrderedDict
+import traceback
 
 # 参数加载区
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -32,6 +36,12 @@ class tpb_proc_class(object):
         setSeqInitVal = "select setval('" + seq_name + "',0);"
         self.mysqlConn.execute(setSeqInitVal.encode('utf-8'))
 
+    def Get_SEQ_Next_Val(self, seq_name):
+        getSeqNextVal = "select nextval('" + seq_name + "');"
+        listsql = self.mysqlConn.execute(getSeqNextVal.encode('utf-8'))
+        seqVal = int(listsql)
+        return seqVal
+
     def Get_DB_Dict(self, rs_group):
         if rs_group == "ALL":
             sqlStr = "select dict_id from sys_dict where dict_the = 'tpb';"
@@ -43,3 +53,76 @@ class tpb_proc_class(object):
             groupList.append(singData["dict_id"])
         return groupList
 
+    def format_datetime(self, up_datetime):
+        # 11-22&nbsp;2016
+        # 02-06&nbsp;18:05
+        # Y-day&nbsp;23:36
+        # Today&nbsp;02:02
+        # <b>7&nbsp;mins&nbsp;ago</b>
+        # 10-17&nbsp;2008
+        if 'Y-day' in up_datetime:
+            today = datetime.date.today()
+            oneday = datetime.timedelta(days=1)
+            yesterday = today - oneday
+            up_datetime = yesterday.strftime('%Y-%m-%d') + " " + up_datetime[-5:]+":00"
+        elif 'ago' in up_datetime:
+            today = datetime.datetime.now()
+            oneday = datetime.timedelta(hours=8)
+            yesterday = today - oneday
+            mins = re.sub("\D", "",up_datetime[0:9])
+            oneday = datetime.timedelta(minutes=int(mins))
+            yesterday = yesterday-oneday
+            up_datetime = yesterday.strftime('%Y-%m-%d %H:%M:%S')
+        elif 'Today' in up_datetime:
+            up_datetime = datetime.datetime.now().strftime('%Y-%m-%d') + " " + up_datetime[-5:]+":00"
+        elif ' 201' in up_datetime:
+            up_datetime = up_datetime[-4:] + "-" + up_datetime[0:2] + "-" + up_datetime[3:5] + " " + "00:00:00"
+        elif ' 202' in up_datetime:
+            up_datetime = up_datetime[-4:] + "-" + up_datetime[0:2] + "-" + up_datetime[3:5] + " " + "00:00:00"
+        elif ' 200' in up_datetime:
+            up_datetime = up_datetime[-4:] + "-" + up_datetime[0:2] + "-" + up_datetime[3:5] + " " + "00:00:00"
+        else:
+            up_datetime = datetime.datetime.now().strftime('%Y') + "-" + up_datetime[0:2] + "-" + up_datetime[3:5] + " " + up_datetime[-5:]+":00"
+        return up_datetime
+
+    def table_row_is_exist(self, tableName, whereDict):
+        ''' 返回指定条件下返回的ROW数据总数 '''
+        strSql = "SELECT * FROM {0} WHERE 0=0".format(tableName)
+        strWhere = ""
+        for key, value in whereDict.items():
+            strWhere = strWhere + " " + "AND {0}='{1}'".format(key, value)
+
+        strSql = strSql + strWhere
+
+        rtnDatas = OrderedDict()
+
+        # print(strSql)
+        rtnDBDatas = self.mysqlConn.query(strSql)
+
+        rtnDatas["CNT"] = len(rtnDBDatas)
+        rtnDatas["CONTENT"] = rtnDBDatas
+        # rtnDatas["STRWHERE"] = strWhere.replace(strWhere[0:4], "")
+        rtnDatas["STRSQL"] = strSql
+
+        return rtnDatas
+
+    def insert_row_table(self, tableName, tpbDict):
+        fieldList = ""
+        valueList = ""
+        for key, value in tpbDict.items():
+            if fieldList == "":
+                fieldList = "{0}".format(key)
+                valueList = "'{0}'".format(value)
+            else:
+                fieldList = "{0}, {1}".format(fieldList, key)
+                valueList = "{0}, '{1}'".format(valueList, value)
+        strSql = "INSERT INTO {0} ({1}) VALUES ({2});".format(tableName, fieldList, valueList)
+        # print(strSql)
+        try:
+            self.mysqlConn.execute(strSql)
+            return True
+        except Exception as e:
+            print(strSql)
+            traceback.print_exc()
+            print(e)
+            return e
